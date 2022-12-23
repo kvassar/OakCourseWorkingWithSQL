79475d29757f3bc7d5332cd82e099a1b0f8fab68
Kal Vassar <kvassar@hbs.net>
2022-12-23 15:20:23 -0600

    Add project files.

---
+++ BLL/UserBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class UserBLL
+    {
+        UserDAO userdao = new UserDAO();
+        public UserDTO GetUserWithUsernameAndPassword(UserDTO model)
+        {
+            UserDTO dto = new UserDTO();
+            dto = userdao.GetUserWithUsernameAndPassword(model);
+            return model;
+        }
+    }
+}
